0300213b95c5f3d4b2353bb8b04c39a18bc5ceb9
Induwara Fernando9 <induwarafernando9@gmail.com>
2023-10-12 22:22:09 +0530

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginPage());
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Column(children: [Text("Login Page")]),
+    );
+  }
+}
