bfd9a790e9c9c9dc99e7c6292d6c9db321fb1824
Induwara Fernando9 <induwarafernando9@gmail.com>
2023-10-14 21:12:42 +0530

    1

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,14 +15,96 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 154, 242, 155),
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(children: [
-          Text(
-            "Login Page",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-        ]),
+        child: Center(
+            child: Column(
+          children: [
+            //Hello again
+            Text(
+              "Hello Again!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Welcome back to the app",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 20),
+
+            //email textfield
+
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 114, 114, 234),
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email",
+                      contentPadding: EdgeInsets.fromLTRB(10, 18.5, 0, 5),
+                      hintStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )),
+
+            SizedBox(height: 20),
+
+            //password textfield
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 114, 114, 234),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Password",
+                    contentPadding: EdgeInsets.fromLTRB(10, 18.5, 0, 5),
+                    hintStyle: TextStyle(color: Colors.white),
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            //sign in button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 82, 51, 194),
+                ),
+                child: Center(
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
